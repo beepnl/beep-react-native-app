4d3215f69299e86561dbc3bb4a57b850385d3780
Maarten van Dillen <mvandillen@gmail.com>
2022-05-19 16:59:49 +0200

    removed theme helpers to prevent circular reference

--- App/Theme/index.ts
+++ App/Theme/index.ts
@@ -3,6 +3,5 @@ import Fonts from './Fonts'
 import Metrics from './Metrics'
 import Images from './Images'
 import ApplicationStyles from './ApplicationStyles'
-import Helpers from './Helpers'
 
-export { Colors, Fonts, Images, Metrics, ApplicationStyles, Helpers }
+export { Colors, Fonts, Images, Metrics, ApplicationStyles }
