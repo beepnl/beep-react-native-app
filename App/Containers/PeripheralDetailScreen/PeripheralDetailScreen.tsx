cb7faaad576fe51f6f453dbc2f37c96881cf39dd
Maarten van Dillen <mvandillen@gmail.com>
2022-05-25 11:50:50 +0200

    fixes for connect/disconnect to peripheral from device name

--- App/Containers/PeripheralDetailScreen/PeripheralDetailScreen.tsx
+++ App/Containers/PeripheralDetailScreen/PeripheralDetailScreen.tsx
@@ -63,16 +63,26 @@ const PeripheralDetailScreen: FunctionComponent<Props> = ({
   const peripheral: PairedPeripheralModel = useTypedSelector<PairedPeripheralModel>(getPairedPeripheral)
   const device = route.params?.device
   const peripheralEqualsDevice = peripheral?.name === device?.name
+  const [error, setError] = useState("")
+  const [busy, setBusy] = useState(false)
+  const isConnected = peripheral && peripheral.isConnected
 
   const onToggleConnectionPress = () => {
-    if (peripheral) {
+    setError("")
+    if (isConnected) {
       BleHelpers.disconnectPeripheral(peripheral)
-      dispatch(BeepBaseActions.setPairedPeripheral(undefined))
+      dispatch(BeepBaseActions.setPairedPeripheral({ ...peripheral, isConnected: false }))
     } else {
+      setBusy(true)
       BleHelpers.scanPeripheralByName(device.name).then((peripheral: Peripheral) => {
         BleHelpers.connectPeripheral(peripheral.id).then(() => {
-          dispatch(BeepBaseActions.setPairedPeripheral(peripheral))
+          dispatch(BeepBaseActions.setPairedPeripheral({ ...peripheral, isConnected: true }))
+          setBusy(false)
         })
+      }).catch(() => {
+        //peripheral not found
+        setError("Device not found")
+        setBusy(false)
       })
     }
     //OK:
@@ -121,15 +131,23 @@ const PeripheralDetailScreen: FunctionComponent<Props> = ({
 
       <View style={styles.spacer} />
       
-      <TouchableOpacity style={styles.button} onPress={onToggleConnectionPress}>
+      <TouchableOpacity style={styles.button} onPress={onToggleConnectionPress} disabled={busy} >
         <Text style={styles.text}>{t(`peripheralDetail.bleConnect.${peripheral ? !peripheral.isConnected : true}`)}</Text>
       </TouchableOpacity>
       <View style={styles.spacer} />
-      <Text style={styles.instructions}>{t("peripheralDetail.instructions")}</Text>
+      { !!busy && <Text>Scanning...</Text> }
+      { !!error && <>
+        <Text style={styles.error}>{error}</Text>
+        <View style={styles.spacer} />
+      </>}
+      { !isConnected && <>
+        <Text style={styles.instructions}>{t("peripheralDetail.instructions")}</Text>
+        <View style={styles.spacer} />
+      </>}
 
-      <View style={styles.spacerDouble} />
+      <View style={styles.spacer} />
 
-      { peripheral && <>
+      { isConnected && <>
         <Text style={styles.label}>{t("peripheralDetail.details")}</Text>
         { MENU_ITEMS.map((item: MenuItem, index: number) => renderMenuItem(item, index.toString()))}
       </>}
