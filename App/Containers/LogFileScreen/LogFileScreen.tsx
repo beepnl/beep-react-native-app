08817185ac93373be26f77f95c4da375dd1b1b26
Maarten van Dillen <mvandillen@gmail.com>
2022-06-02 16:12:00 +0200

    improved log file screen

--- App/Containers/LogFileScreen/LogFileScreen.tsx
+++ App/Containers/LogFileScreen/LogFileScreen.tsx
@@ -31,6 +31,7 @@ import ScreenHeader from '../../Components/ScreenHeader'
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { ScrollView } from 'react-native-gesture-handler';
 import ApiService from '../../Services/ApiService';
+import * as Progress from 'react-native-progress';
 
 interface Props {
 }
@@ -57,8 +58,8 @@ const LogFileScreen: FunctionComponent<Props> = ({
   useEffect(() => {
     if (logFileProgress > 0 && logFileProgress === logFileSize?.value()) {
       //download finished, copy to SD card
-      // BleHelpers.exportLogFile()
-      
+      // BleHelpers.exportLogFile()   //when uncommenting, also uncomment permission request in onDownloadLogFilePress()
+
       // dispatch(BeepBaseActions.setLogFileProgress(0))
       //download finished, upload to api
       RNFS.uploadFiles({
@@ -78,14 +79,14 @@ const LogFileScreen: FunctionComponent<Props> = ({
           "id": peripheral.deviceId,
         },
         begin: (response: UploadBeginCallbackResult) => setUploadProgress(0),
-        progress: (response: UploadProgressCallbackResult) => setUploadProgress(Math.round((response.totalBytesSent/response.totalBytesExpectedToSend) * 100))
+        progress: (response: UploadProgressCallbackResult) => setUploadProgress(response.totalBytesSent / response.totalBytesExpectedToSend)
       }).promise.then((response: UploadResult) => {
         if (response.statusCode == 200) {
           console.log('FILES UPLOADED!'); // response.statusCode, response.headers, response.body
-          setUploadProgress(1000)
+          setUploadProgress(1)
         } else {
           setUploadProgress(0)
-          console.log('SERVER ERROR');          //'{"lines_received":2,"bytes_received":66152,"log_size_bytes":null,"log_has_timestamps":0,"log_saved":true,"log_parsed":false,"log_messages":0,"erase_mx_flash":-1,"erase":false,"erase_type":"fatfs"}\n'
+          console.log('SERVER ERROR');
           setUploadStatus(`${response.statusCode}: ${response.body}`)
         }
       })
@@ -108,20 +109,20 @@ const LogFileScreen: FunctionComponent<Props> = ({
 
   const onDownloadLogFilePress = async () => {
     if (logFileSize) {
-      PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE).then(async (granted: boolean) => {
-        let requested
-        if (!granted) {
-          requested = await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-            {
-              title: "BEEP Base",
-              message: "Please grant access to write to storage",
-              buttonNegative: "Cancel",
-              buttonPositive: "OK"
-            }
-          );
-        }
-        if (granted || requested === PermissionsAndroid.RESULTS.GRANTED) {
+      // PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE).then(async (granted: boolean) => {
+      //   let requested
+      //   if (!granted) {
+      //     requested = await PermissionsAndroid.request(
+      //       PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+      //       {
+      //         title: "BEEP Base",
+      //         message: "Please grant access to write to storage",
+      //         buttonNegative: "Cancel",
+      //         buttonPositive: "OK"
+      //       }
+      //     );
+      //   }
+      //   if (granted || requested === PermissionsAndroid.RESULTS.GRANTED) {
           setUploadProgress(0)
           setUploadStatus("")
           dispatch(BeepBaseActions.clearLogFileFrames())
@@ -129,9 +130,14 @@ const LogFileScreen: FunctionComponent<Props> = ({
             BleHelpers.initLogFile()
             BleHelpers.write(peripheral.id, [0x20, 0x00, 0x00, 0x00, 0x00])
           }
-        }
-      })
+        // }
+      // })
     }
+  }
+
+  let downloadProgress = logFileProgress / logFileSize?.value()
+  if (isNaN(downloadProgress)) {
+    downloadProgress = 0
   }
 
   return (<>
@@ -147,20 +153,44 @@ const LogFileScreen: FunctionComponent<Props> = ({
       <TouchableOpacity style={styles.button} onPress={onDownloadLogFilePress} disabled={logFileSize == undefined || logFileSize.value() == 0} >
         <Text style={styles.text}>{t("logFile.downloadLogFile")}</Text>
       </TouchableOpacity>
+      
+      <View style={styles.spacerDouble} />
+      <Text style={styles.label}>{t("logFile.progress")}</Text>
       <View style={styles.spacer} />
-      <Text style={[styles.text]}>{`Download progress: ${Math.round(logFileProgress / logFileSize?.value() * 100)} %`}</Text>
+
+      <View style={{ flexDirection: "row", flex: 1, justifyContent: "space-between", alignItems: "center" }}>
+        <View>
+          <Text style={[styles.text]}>{t("logFile.download")}</Text>
+          <View style={styles.spacer} />
+          <Text style={[styles.text]}>{t("logFile.upload")}</Text>
+        </View>
+        <View style={styles.spacer} />
+        <View>
+          <Progress.Bar progress={downloadProgress} width={150} height={20} color={Colors.yellow} borderColor={Colors.black} borderRadius={8} />
+          <View style={styles.spacer} />
+          <Progress.Bar progress={uploadProgress} width={150} height={20} color={Colors.yellow} borderColor={Colors.black} borderRadius={8} />
+        </View>
+        <View style={styles.spacer} />
+        <View>
+          <Text style={[styles.text]}>{`${Math.round(downloadProgress * 100)} %`}</Text>
+          <View style={styles.spacer} />
+          <Text style={[styles.text]}>{`${Math.round(uploadProgress * 100)} %`}</Text>
+        </View>
+      </View>
+
       <View style={styles.spacer} />
-      <TextInput 
+
+      {/* <TextInput 
         style={[styles.text, { height: 300, borderWidth: 1, borderColor: Colors.yellow }]} 
         multiline={true} 
         editable={false}
         scrollEnabled={true}
         value={combinedLogFileFrames.toString("hex")}
       />
+      <Text style={[styles.text]}>{`Upload progress: ${uploadProgress} %`}</Text>
+      */}
 
       <View style={styles.spacer} />
-
-      <Text style={[styles.text]}>{`Upload progress: ${uploadProgress} %`}</Text>
       <Text style={[styles.text]}>{`Upload status: ${uploadStatus}`}</Text>
 
     </ScrollView>
