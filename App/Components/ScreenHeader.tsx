099b71edb12a1bf19d0a277bbd5ba64633530ac8
Maarten van Dillen <mvandillen@gmail.com>
2022-06-16 09:30:35 +0200

    fixed menu font size

--- App/Components/ScreenHeader.tsx
+++ App/Components/ScreenHeader.tsx
@@ -111,7 +111,7 @@ const styles = StyleSheet.create({
   },
 
   menuItem: {
-    ...Fonts.style.big,
+    ...Fonts.style.regular,
     marginHorizontal: Metrics.doubleBaseMargin,
     color: Colors.text,
   },
