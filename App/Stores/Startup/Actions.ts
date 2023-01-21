7a2a5813b81733957d11cd2b9853a9d2b8aa4c4b
ezja <change@this.com>
2023-01-21 10:58:57 +0100

    Synchronize clock immediately after connecting to device.

--- App/Stores/Startup/Actions.ts
+++ App/Stores/Startup/Actions.ts
@@ -15,6 +15,15 @@ const CreatedActions = createActions( {
   startup: null
 } );
 
+  /*
+  // RESET CLOCK
+  */
+    
+  params = Buffer.alloc(4)
+  params.writeUint32BE((new Date().valueOf() + 1300) / 1000, 0)
+  yield call(BleHelpers.write, peripheralId, COMMANDS.WRITE_CLOCK, params)
+
+
 export default CreatedActions.Creators as C;
 
  
