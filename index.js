5e36d993baa444752115284d8f1ea26242234bd8
Maarten van Dillen <mvandillen@gmail.com>
2024-09-10 14:56:00 +0200

    fixed Buffer implementation

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ import {name as appName} from './app.json';
 
 if (typeof Buffer === 'undefined') global.Buffer = require('buffer').Buffer
 
+// Fix when Buffer.subarray is not a Buffer (see also https://github.com/feross/buffer/issues/329)
+if (!(Buffer.alloc(1).subarray(0, 1) instanceof Buffer)) {
+  Buffer.prototype.subarray = function subarray() {
+    const result = Uint8Array.prototype.subarray.apply(this, arguments);
+    Object.setPrototypeOf(result, Buffer.prototype);
+    return result;
+  };
+}
+
 AppRegistry.registerComponent(appName, () => App);
